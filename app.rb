418fd070e6133a4761158fe64192f1788bc0c02d
sreelekhab77 <133074494+SreelekhaB77@users.noreply.github.com>
2024-12-12 15:13:42 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-# testing2
 require 'sinatra'
 require 'geocoder'
 require 'json'
@@ -13,7 +12,7 @@ get '/user_location' do
   user_ip = request.ip
 
   # Fallback IP for local testing (use a real IP when deployed)
-  user_ip = '8.8.8.8' if user_ip == '34.229.146.80'
+   user_ip = '8.8.8.8' if user_ip == '54.160.215.88'
 
   # Use Geocoder to get location data
   location_data = Geocoder.search(user_ip).first
@@ -30,23 +29,4 @@ get '/user_location' do
     status 404
     { error: 'Unable to determine location' }.to_json
   end
-end
-
-
-# Webhook listener for GitHub events
-post '/webhook' do
-  # Verify the webhook signature
-  request_body = request.body.read
-  secret = ENV['08fcff977a287a55c9d921779171886c1edffe3e']  # Secret token set in your environment
-  signature = 'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, request_body)
-
-  halt 403, "Invalid signature" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE_256'])
-
-  # Process the webhook payload
-  payload = JSON.parse(request_body)
-  if payload['ref'] == 'refs/heads/main'  # Check for pushes to the main branch
-    system("cd /home/ubuntu/project/geo_tracker && git pull origin main && bundle install && restart_app.sh")
-  end
-
-  "Webhook received and processed!"
 end
